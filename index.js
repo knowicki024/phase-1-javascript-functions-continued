d75bddc9ec1c34f03a9723a2d56eed1914f0db45
Kaitlyn Nowicki <katie.nowicki024@gmail.com>
2023-11-18 21:02:10 -0500

    homework

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // code your solution here
+//test 1
+function saturdayFun(rollerskate = "roller-skate") {
+    return `This Saturday, I want to ${rollerskate}!`;
+}
+
+console.log(saturdayFun());
+
+//test 2
+const mondayWork = function(activity) {
+    if (activity) {
+        return `This Monday, I will ${activity}.`;
+     } else {
+            return "This Monday, I will go to the office.";
+        }}
+
+console.log(mondayWork());
+
+//test 3
+function wrapAdjective(highlight = "*") {
+    return function(encouragement = "a hard worker") {
+return `You are ${highlight}${encouragement}${highlight}!`
+}
+}
+
